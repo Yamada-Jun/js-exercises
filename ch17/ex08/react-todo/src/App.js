e00616eb9971f4690e673bdaa93ea6cb49203db7
Jun Yamada <jun.yamada@jp.ricoh.com>
2026-03-25 23:48:23 +0900

    useStateを使って、inputの内容を保持する

--- ch17/ex08/react-todo/src/App.js
+++ ch17/ex08/react-todo/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+    const [val, setVal] = useState(""); //useStateを使う。useStateは、引数に初期値を渡すと、現在の値と、その値を更新するための関数を返す。
+
     return (
         <>
             <form id="new-todo-form">
-                <input type="text" id="new-todo" placeholder="What needs to be done?" />
+                <input type="text" id="new-todo" placeholder="What needs to be done?"
+                    value={val}
+                    onChange={(e) => setVal(e.target.value)} /> {/* イベントハンドラの利用。inputの値が変更されたときに、setVal関数を呼び出して、valの値を更新する*/ }
                 <button type="submit">Add</button>
             </form>
             <ul id="todo-list"></ul>
