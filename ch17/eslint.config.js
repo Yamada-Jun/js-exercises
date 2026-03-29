051ad7b9604ae65fe9306dda4619e76a2dae9bf1
Jun Yamada <jun.yamada@jp.ricoh.com>
2026-03-29 23:12:31 +0900

    ESLintとPrettierの設定ファイルを作成

---
+++ ch17/eslint.config.js
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+    {
+        ignores: [
+            'ch01/format_sample.js'
+        ]
+    },
+
+    {
+        files: [
+            "**/*.{js,mjs,cjs}"
+        ],
+        plugins: { js },
+        extends: ["js/recommended"],
+        languageOptions: { globals: globals.browser }
+    },
+
+    prettier
+]);
