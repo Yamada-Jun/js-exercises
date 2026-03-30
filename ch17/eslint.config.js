df4ca1a7401560ee7ef09963ceaabbae4d5fce70
Jun Yamada <jun.yamada@jp.ricoh.com>
2026-03-30 17:13:47 +0900

    問題17.1実施

--- ch17/eslint.config.js
+++ ch17/eslint.config.js
@@ -1,23 +1,38 @@
-import js from "@eslint/js";
+﻿import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
+import google from 'eslint-config-google';
 import prettier from 'eslint-config-prettier';
+import { defineConfig } from "eslint/config";
 
 export default defineConfig([
     {
         ignores: [
-            'ch01/format_sample.js'
-        ]
-    },
+            'ex01/format_sample.js' // format_sample.jsを対象型外す
+        ],
 
-    {
         files: [
             "**/*.{js,mjs,cjs}"
         ],
-        plugins: { js },
-        extends: ["js/recommended"],
-        languageOptions: { globals: globals.browser }
+
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+
+            globals: {
+                ...globals.browser, // ブラウザ環境のグローバル変数を許可
+                ...globals.node,    // Node.js環境のグローバル変数を許可
+            },
+        },
+
+        rules: {
+            ...js.configs.recommended.rules,    // ESLintの推奨ルールを適用
+            ...google.rules,                    // Googleのスタイルガイドのルールを適用
+
+            'require-jsdoc': 'off',             // TypeError対応
+            'valid-jsdoc': 'off',               // TypeError対応
+        },
     },
 
-    prettier
+
+    prettier    // prettierとの競合対応
 ]);
